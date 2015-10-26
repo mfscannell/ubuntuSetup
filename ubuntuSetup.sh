d050ce3b4d9294b1d5477dbcfdaf1748633f218e
Mark Scannell <mfscannell@users.noreply.github.com>
2015-10-26 14:06:23 -0500

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -1,4 +1,17 @@
-sudo apt-get install gcc
-sudo aptiget install g++
+sudo apt-get update
 
+echo "Installing C/C++ compilers"
+sudo apt-get install gcc
+sudo apt-get install g++
+
+echo "Installing NodeJS"
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install nodejs-legacy
+sudo apt-get install -g bower
+
+echo "Installing MongoDB"
+sudo apt-key adv -keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get install mongodb-org
+sudo apt-get install mongodb-clients
