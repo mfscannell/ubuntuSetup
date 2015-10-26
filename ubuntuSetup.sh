26be6c4b39e19fbf6f9d2c3075cff8e0a770b49b
Mark Scannell <mfscannell@users.noreply.github.com>
2015-10-26 14:09:30 -0500

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -12,6 +12,6 @@ sudo apt-get install -g bower
 
 echo "Installing MongoDB"
 sudo apt-key adv -keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get install mongodb-org
 sudo apt-get install mongodb-clients
